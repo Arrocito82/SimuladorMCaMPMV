ef30874466efad9619bcde11510e5f59be700a5e
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2022-11-16 23:34:05 -0600

    reparación de items de dirección memoria cache

--- Assets/Scripts/MemoriaCache.cs
+++ Assets/Scripts/MemoriaCache.cs
@@ -5,23 +5,30 @@ using UnityEngine.UI;
 using System;
 public class MemoriaCache : MonoBehaviour
 {
-    private List<Tuple<int, int, int, int, int, int, int, GameObject>> direccionMemoriaCache;
+    private List<Tuple<int, int, GameObject, Tuple<int, int, int, int>, Tuple<int, int, int, int>>> direccionMemoriaCache;
     private GameObject direccionTemplate;
     private int bloque, linea, dato0, dato1, dato2, dato3, dato4, dato5, dato6, dato7;
     [SerializeField] private int maximoDireccionableMC;
     private void Awake()
     {
-        direccionMemoriaCache = new List<Tuple<int, int, int, int, int, int, int,GameObject>>();
+        direccionMemoriaCache = new List<Tuple<int, int, GameObject, Tuple<int, int, int, int>, Tuple<int, int, int, int>>>();
         direccionTemplate=this.transform.GetChild(0).gameObject;
         //inicializando memoria cache
         for (int i=0x0; i< maximoDireccionableMC; i++)
         {
             GameObject direccionItem = Instantiate(direccionTemplate, this.transform);
-            direccionItem.transform.GetChild(0).GetComponent<Text>().text = $"{i:X}";
-            direccionItem.transform.GetChild(1).GetComponent<Text>().text = $"{i:X}";
-            direccionItem.transform.GetChild(2).GetComponent<Text>().text = $"{i:X}";
-           
-            direccionMemoriaCache.Add(new Tuple<int, int, int, int, int, int, int,GameObject>(i, i,i,i,i,i,i, direccionItem));
+            direccionItem.transform.GetChild(0).GetComponent<Text>().text = $"{i:X2}";
+            direccionItem.transform.GetChild(1).GetComponent<Text>().text = $"{i:X1}";
+            direccionItem.transform.GetChild(2).GetComponent<Text>().text = $"{i:X2}";
+            direccionItem.transform.GetChild(3).GetComponent<Text>().text = $"{i:X2}";
+            direccionItem.transform.GetChild(4).GetComponent<Text>().text = $"{i:X2}";
+            direccionItem.transform.GetChild(5).GetComponent<Text>().text = $"{i:X2}";
+            direccionItem.transform.GetChild(6).GetComponent<Text>().text = $"{i:X2}";
+            direccionItem.transform.GetChild(7).GetComponent<Text>().text = $"{i:X2}";
+            direccionItem.transform.GetChild(8).GetComponent<Text>().text = $"{i:X2}";
+            direccionItem.transform.GetChild(9).GetComponent<Text>().text = $"{i:X2}";
+            Tuple<int, int, int, int> datos = new Tuple<int, int, int, int>(i, i, i, i);
+            direccionMemoriaCache.Add(new Tuple<int, int, GameObject, Tuple<int, int, int, int>, Tuple<int, int, int, int>> (i, i, direccionItem, datos, datos));
         }
         Destroy(direccionTemplate);
     }
