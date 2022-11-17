6189a3fbefbaab309176eb6c6235faf4397b6bcc
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2022-11-16 21:28:15 -0600

    memoria virtual se llena inicialmente con datos aleatorios

--- Assets/Scripts/MemoriaVirtual.cs
+++ Assets/Scripts/MemoriaVirtual.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = System.Random;
 
 public class MemoriaVirtual : MonoBehaviour
 {
@@ -14,14 +15,39 @@ public class MemoriaVirtual : MonoBehaviour
         direccionesMemoriaVirtual = new List<Tuple<int, int, GameObject>>();
         //recuperando el primer elemento que servira de template
         direccionTemplate = this.transform.GetChild(0).gameObject;
+        /* 
+         * INICIALIZANDO MEMORIA VIRTUAL
+         * El desplazamiento cuenta las palabras dentro de cada página, en este caso hay 32. 
+         * Mismo caso aplica para la página, dado que hay 32 páginas dentro de la memoria virtual.
+         * Sin embargo, debe ser en hexadecimal de 5 bits. 
+         * 32 decimal = 20 hexadecimal.
+         * Con el fin de tener datos de prueba se llenara toda la memoria virtual de datos aleatoriamente
+        */
+        int contadorDesplazamiento = 0x0; 
+        int contadorPagina = 0x0;
+        Random dato = new Random();
 
-        //inicializando memoria virtual
         for (int i=0x0; i< maximoDireccionableMV; i++)
         {
             GameObject direccionItem = Instantiate(direccionTemplate, this.transform);
-            direccionItem.transform.GetChild(0).GetComponent<Text>().text = $"{i:X}";
-            direccionItem.transform.GetChild(1).GetComponent<Text>().text = $"{i:X}";
-            direccionesMemoriaVirtual.Add(new Tuple<int,int, GameObject>(i, i, direccionItem));
+            direccionItem.transform.GetChild(0).GetComponent<Text>().text = $"{contadorPagina:X5}";
+            direccionItem.transform.GetChild(1).GetComponent<Text>().text = $"{contadorDesplazamiento:X5}";
+            direccionItem.transform.GetChild(2).GetComponent<Text>().text = $"{dato.Next():X5}";
+            direccionesMemoriaVirtual.Add(new Tuple<int,int, GameObject>(contadorPagina, contadorDesplazamiento, direccionItem));
+
+            // set contadores
+            if (contadorDesplazamiento < 0x1f)
+            {
+                //recorriendo página
+                contadorDesplazamiento++;
+            }
+            else
+            {
+                // reiniciando a la posición 0 de la siguiente página
+                contadorDesplazamiento=0x0;
+                contadorPagina++;
+            }
+
         }
         Destroy(direccionTemplate);
         
@@ -40,7 +66,7 @@ public class MemoriaVirtual : MonoBehaviour
     }
     //public void addDireccionMemoriaVirtual(int pagina, int desplazamiento)
     //{
-        
+
     //    DireccionMemoriaVirtual currentDireccion = new DireccionMemoriaVirtual(pagina, desplazamiento);
     //    //direccionesMemoriaVirtual.Add(currentDireccion);// eliminando de la lista
     //}
